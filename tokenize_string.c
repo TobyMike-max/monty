6f3916e172918b3bd38f5c2eb52513d244631c41
TobyMike-max <osinubi.michael@gmail.com>
2022-06-18 05:15:57 +0100

    [Create]: Function to tokenize string

---
+++ tokenize_string.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+char **strtow(char *str, char *delims);
+int is_delim(char ch, char *delims);
+int get_word_length(char *str, char *delims);
+int get_word_count(char *str, char *delims);
+char *get_next_word(char *str, char *delims);
+
+/**
+ * strtow - takes a string and separates its words
+ *
+ * @str: string to separate into words
+ * @delims: delimitors to use to delimit words
+ *
+ * Return: 2D array of pointers to each word
+ */
+char **strtow(char *str, char *delims)
+{
+	char **words = NULL;
+	int wc, wordLen, n, i = 0;
+
+	if (str == NULL || !*str)
+		return (NULL);
+	wc = get_word_count(str, delims);
+
+	if (wc == 0)
+		return (NULL);
+
